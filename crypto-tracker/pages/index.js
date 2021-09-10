b4951789e418bfa3c63e5bdff38b6bc7f6de83e6
Satyam <ss9125rpvv@gmail.com>
2021-09-11 04:29:23 +0530

    first commit

---
+++ crypto-tracker/pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Crypto Tracker</title>
+      </Head>
+      <h1>Crypto Tracker</h1>
+    </div>
+  )
+}
